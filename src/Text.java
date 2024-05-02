861828fd7fdd20d76e6ddf6e0f12ec7c51c78259
matti <mattihro105@gmail.com>
2024-05-02 14:30:47 +0200

    text verarbeiter

---
+++ src/Text.java
@@ -0,0 +1,24 @@
+import java.lang.String;
+import java.lang.StringBuilder;
+import java.io.*;
+import java.io.FileNotFoundException;
+
+public class Text {
+
+    private String pfad;
+
+    public Text(String pfad) {
+        this.pfad = pfad;
+    }
+
+    private String readTextFromFile() throws FileNotFoundException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(pfad))){
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e.getMessage());
+        }
+        return pfad;
+    }
+}
