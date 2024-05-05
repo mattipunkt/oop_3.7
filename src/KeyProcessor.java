36dbf4afb7439b0449c5aa884b30c21b4e411f01
flo <hasselbacher.florian@gmail.com>
2024-05-05 19:09:07 +0200

    KeyGenerator angepasst und  array return eingearbeitet

--- src/KeyProcessor.java
+++ src/KeyProcessor.java
@@ -1,6 +1,22 @@
 public class KeyProcessor {
 
-    public void encode() {
+    public int[] textToInt(String text){
+        int [] tempNum = new int[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            tempNum[i] = Character.getNumericValue(text.charAt(i));
+        }
+        return tempNum;
+    }
+    public String intToText(int[] tempNum){
+        StringBuilder convert = new StringBuilder();
+        for (int i = 0; i < tempNum.length; i++) {
+            convert.append(tempNum[i]);
+            convert.append(":");
+        }
+        String converted = convert.toString();
+        return converted;
+    }
+    public void encode(String text) {
 
     }
 
