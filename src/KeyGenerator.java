5f520cbb5ff6ece23a8aada9066d254c7e751a20
flo <hasselbacher.florian@gmail.com>
2024-05-05 18:18:39 +0200

    KeyGenerator angepasst und  array return eingearbeitet

--- src/KeyGenerator.java
+++ src/KeyGenerator.java
@@ -7,18 +7,24 @@ public class KeyGenerator {
     public KeyGenerator(int p, int q) {
         this.p = p;
         this.q = q;
+        this.g = p * q;
 
 
     }
 
     public void generatePrivateKey() {
-        g = p * q;
         int phi = (p-1)*(q-1);
         int e = findE(phi);
         int d = findD(e, phi);
-        System.out.println("Der Öffentliche Schlüssel ist: {" + e + ", " + g +"}");
         System.out.println("Der Private Schlüssel ist: {" + d + ", " + g +"}");
 
+    }
+    public int[] generatePublicKey() {
+        int phi = (p-1)*(q-1);
+        int e = findE(phi);
+        int[] pubKey = new int[]{e, g};
+        System.out.println("Der Öffentliche Schlüssel ist: {" + e + ", " + g +"}");
+        return pubKey;
     }
 
     public int ggT(int a, int b) {
